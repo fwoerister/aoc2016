7e2f02be18ca7d99a049a9bfa69497807740dd1a
Florian Wörister <florian.woerister@univie.ac.at>
2025-07-23 16:42:26 +0200

    day 25 added

---
+++ src/day25.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <regex>
+
+#include <memory>
+
+#include "util/emulator.hpp"
+#include "util/instructions.hpp"
+
+using namespace std;
+
+int main()
+{
+    ifstream f("input/day25.txt");
+    string line;
+
+    vector<unique_ptr<Instruction>> program;
+
+    regex r(R"((\w+) (a|b|c|d|-?\d+) ?(a|b|c|d|-?\d+)?)");
+    smatch m;
+
+    while (getline(f, line))
+    {
+        regex_search(line, m, r);
+
+        if (m[1] == "cpy")
+        {
+            program.push_back(make_unique<CpyInstruction>(m[2], m[3]));
+        }
+        else if (m[1] == "inc")
+        {
+            program.push_back(make_unique<IncInstruction>(m[2]));
+        }
+        else if (m[1] == "dec")
+        {
+            program.push_back(make_unique<DecInstruction>(m[2]));
+        }
+        else if (m[1] == "jnz")
+        {
+            program.push_back(make_unique<JnzInstruction>(m[2], m[3]));
+        }
+        else if (m[1] == "tgl")
+        {
+            program.push_back(make_unique<ToggleInstruction>(m[2]));
+        }
+        else if (m[1] == "out")
+        {
+            program.push_back(make_unique<OutInstruction>(m[2]));
+        }
+    }
+
+    int a = 0;
+
+    while (true)
+    {
+        Emulator e(program);
+        e.setRegisterVal("a", a);
+
+        while (e.getOutputSize() < 100 && e.isClock())
+        {
+            e.step();
+        }
+
+        if (e.isClock())
+        {
+            cout << a << endl;
+            break;
+        }
+        else
+        {
+            a++;
+        }
+    }
+}
